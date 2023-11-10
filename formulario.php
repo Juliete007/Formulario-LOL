de2eda4a821cb6f4d39ce23b98a6f8e5c7d66fc7
Julio Francés Prieto <111543050+Juliete007@users.noreply.github.com>
2023-11-10 13:47:48 +0100

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Campeones de League of Legends</title>
+    </head>
+    <body>
+        <h1>Resultado de la encuesta sobre nuestros campeones</h1>
+        <?php
+            print("<h1>El formulario de PHP. Resultados del formulario</h1>\n");
+            $nombre = $_POST["user_name"];
+            $apellidos = $_POST["user_surname"];
+            $email = $_POST["user_email"];
+            $ciudad = $_POST["user_city"];
+
+            $campeon = $_POST["user_campeon"];
+            $otrocampeon = $_POST["campeon_dispuesto"];
+            $posicion = $_POST["user_position"];
+
+            //Impresion de los datos recogidos
+            print("<p>Los datos que se han introducido son:</p>");
+            print("<ul>\n");
+            print("<li>Nombre del usuario: $nombre</li>\n");
+            print("<li>Apellidos del usuario: $apellidos</li>\n");
+            print("<li>Email del usuario: $email</li>\n");
+            print("<li>Ciudad del usuario: $ciudad</li>\n");
+            print("<li>Campeón favorito: $campeon</li>\n");
+            print("<li>Otro campeón: $otrocampeon</li>\n");
+            print("<li>Posición favorita: $posicion</li>\n");
+
+            print("</ul>\n");
+        ?>
+    </body>
+</html>
